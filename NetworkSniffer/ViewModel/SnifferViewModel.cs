0d7e6e4b37a540dd5f64860d597de1a1f1080d4a
gcupko00 <gcupko00@fesb.hr>
2016-04-30 19:15:18 +0200

    Minor commit

--- NetworkSniffer/ViewModel/SnifferViewModel.cs
+++ NetworkSniffer/ViewModel/SnifferViewModel.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace NetworkSniffer.ViewModel
 {
     public class SnifferViewModel : ViewModelBase
     {
+        public SnifferViewModel()
+        {
+
+        }
+
+        private string filter;
+
+        public string Filter
+        {
+            get
+            {
+                return filter;
+            }
+            set
+            {
+                filter = value;
+            }
+        }
+
+        public ICommand StartCapture { get; private set; }
+
+        public ICommand StopCapture { get; private set; }
+
 
     }
 }
