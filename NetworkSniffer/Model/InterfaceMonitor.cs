3c74f53ef2d67017bd61fa0c5a610d30f0b213a1
bolkonksy <bolkonsky@tuta.io>
2016-05-03 15:33:39 +0200

    Added InterfaceMonitor

---
+++ NetworkSniffer/Model/InterfaceMonitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetworkSniffer.Model
+{
+    class InterfaceMonitor
+    {
+        private Socket socket;
+        private IPAddress ipAddress;
+
+        public InterfaceMonitor(string ip)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+            ipAddress = IPAddress.Parse(ip);
+        }
+
+        public void StartCapture()
+        {
+            /* Bind the socket to selected IP address */
+            socket.Bind(new IPEndPoint(ipAddress, 0));
+
+            /* Socket options apply only to IP packets */
+            socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+
+            byte[] byteTrue = new byte[4] { 1, 0, 0, 0 };
+            byte[] byteOut = new byte[4];
+            /* ReceiveAll implies that all incoming and outgoing packets on the interface are captured.
+             * Second option should be TRUE */
+            socket.IOControl(IOControlCode.ReceiveAll, byteTrue, byteOut);
+
+            //begin receive
+        }
+
+        public void StopCapture()
+        {
+            socket.Close();
+        }
+    }
+}
--- NetworkSniffer/ViewModel/MainViewModel.cs
+++ NetworkSniffer/ViewModel/MainViewModel.cs
@@ -1,6 +1,12 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Net;
+using NetworkSniffer.Model;
+
+/*testing*/
+using System.Windows;
 
 namespace NetworkSniffer.ViewModel
 {
@@ -10,9 +16,9 @@ namespace NetworkSniffer.ViewModel
     public class MainViewModel : ViewModelBase
     {
         private SnifferViewModel snifferViewModel = new SnifferViewModel();
-
         private AnalyzerViewModel analyzerViewModel = new AnalyzerViewModel();
 
+        private InterfaceMonitor monitor;
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -21,10 +27,13 @@ namespace NetworkSniffer.ViewModel
             CurrentViewModel = snifferViewModel;
             OpenAnalyzer = new RelayCommand(() => OpenAnalyzerExecute());
             OpenSniffer = new RelayCommand(() => OpenSnifferExecute());
+            StartCapture = new RelayCommand(() => StartCaptureExecute());
+
+            DeviceAddressList = new ObservableCollection<string>();
+            GetAddresses();
         }
 
         private ViewModelBase currentViewModel;
-
         public ViewModelBase CurrentViewModel
         {
             get
@@ -35,6 +44,43 @@ namespace NetworkSniffer.ViewModel
             {
                 currentViewModel = value;
                 RaisePropertyChanged("CurrentViewModel");
+            }
+        }
+
+        private ObservableCollection<string> deviceAddressList;
+        public ObservableCollection<string> DeviceAddressList
+        {
+            get
+            {
+                return deviceAddressList;
+            }
+            private set
+            {
+                deviceAddressList = value;
+                RaisePropertyChanged("DeviceAddressList");
+            }
+        }
+
+        private string selectedAddress;
+        public string SelectedAddress
+        {
+            get
+            {
+                return selectedAddress;
+            }
+            set
+            {
+                selectedAddress = value;
+                RaisePropertyChanged("SelectedAddress");
+            }
+        }
+
+        private void GetAddresses()
+        {
+            IPHostEntry HostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            if (HostEntry.AddressList.Length > 0) {
+                foreach (IPAddress ip in HostEntry.AddressList)
+                    DeviceAddressList.Add(ip.ToString());
             }
         }
 
@@ -50,6 +96,13 @@ namespace NetworkSniffer.ViewModel
         private void OpenSnifferExecute()
         {
             CurrentViewModel = snifferViewModel;
+        }
+        
+        public ICommand StartCapture { get; private set; }
+
+        private void StartCaptureExecute()
+        {
+            monitor = new InterfaceMonitor(selectedAddress);
         }
     }
 }
