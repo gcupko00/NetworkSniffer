3b3461d1534c6b3f8d11250efe681cbf695c1eb2
gcupko00 <gcupko00@fesb.hr>
2016-05-16 17:53:05 +0200

    HelpViewModel fix

--- NetworkSniffer/ViewModel/HelpViewModel.cs
+++ NetworkSniffer/ViewModel/HelpViewModel.cs
@@ -11,15 +11,23 @@ using System.Windows;
 
 namespace NetworkSniffer.ViewModel
 {
+    /// <summary>
+    /// This class contains properties that the HelpView can data bind to.
+    /// </summary>
     public class HelpViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Initializes new instance of the HelpViewModel class
+        /// </summary>
         public HelpViewModel()
         {
             try
             {
+                // Stores help document as memory stream
                 MemoryStream memoryResStream = new MemoryStream(Encoding.Default.GetBytes(HelpResource.Help));
 
                 HelpTextBox = new RichTextBox();
+                HelpTextBox.IsReadOnly = true;
                 HelpTextBox.Padding = new Thickness(5);
                 HelpTextBox.SelectAll();
                 HelpTextBox.Selection.Load(memoryResStream, DataFormats.Rtf);
@@ -30,6 +38,9 @@ namespace NetworkSniffer.ViewModel
             }
         }
         
+        /// <summary>
+        /// Used to store help document
+        /// </summary>
         public RichTextBox HelpTextBox { get; private set; }
     }
 }
